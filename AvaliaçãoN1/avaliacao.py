2aeabe7ebec216d940fa0f3dcb297cb5416d2e8e
cristianvitor01 <cristianvitoralves@gmail.com>
2023-04-03 16:05:25 -0300

    centro arvore

--- AvaliaçãoN1/avaliacao.py
+++ AvaliaçãoN1/avaliacao.py
@@ -1,3 +1,19 @@
 """
 Dado uma árvore ( uma árvore é um grafo acíclico!😎 ) calcule o centro dessa árvore.
 """
+
+from Grafo import GrafoListaAdj
+from CentroArvore import CentroArvore 
+
+grafo = GrafoListaAdj(orientado=False)
+
+grafo.DefinirN(4, VizinhancaDuplamenteLigada=True)
+
+e = grafo.AdicionarAresta(1,2)
+e = grafo.AdicionarAresta(1,3)
+e = grafo.AdicionarAresta(1,4)
+
+
+centro = CentroArvore(grafo)
+
+print('O centro da árvore é', centro)
